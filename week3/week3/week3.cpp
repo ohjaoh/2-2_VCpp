1dc540d2ec7c2d860c400d43cab0ad2439fa2805
ohjaoh <ohja1004@naver.com>
2023-09-19 17:05:08 +0900

    [오자현](2309191705) 우측하단a를 동시에 누르면 출력까진 해냄 아마도 배열이 필요할듯함

--- week3/week3/week3.cpp
+++ week3/week3/week3.cpp
@@ -1,36 +1,46 @@
-#include <stdio.h>
+#include <iostream>
+#include <chrono>
 #include <windows.h>
-
+using namespace std; // 표준 네임스페이스에 정의되어 있는 것들을 사용하겠다.
+using namespace std::chrono; // 표준 네임스페이스에 정의된 chrono의 것들을 사용하겠다.
 int main() {
 
 	while (1) {
 		int escPressed = 0;
 		int index = 0;
+		chrono::milliseconds waitTime(300);
+		bool check = false; 
 
-		printf("기술커멘드");
+		cout << "기술커멘드" << endl;
 		while (1) {
-			Sleep(300);
+
 			if (GetAsyncKeyState(VK_ESCAPE)) {// esc키라면 바로 즉시종료
 				escPressed = 1; // esc눌림여부확인
 				break; //입력 while문 탈출
 			}
+
 			if (GetAsyncKeyState(VK_RIGHT)) {
-				printf("우측키입력");
+				time_point<system_clock> startTime = system_clock::now();
+				if (GetAsyncKeyState(VK_DOWN)) {
+					if (GetAsyncKeyState(VK_DOWN)) {
+						if (GetAsyncKeyState('A')) {
+							cout << "아따라야" << endl;
+						}
+					}
+				}
 			}
 			if (GetAsyncKeyState(VK_DOWN)) {
-				printf("아래방향키입력");
 			}
-			if (GetAsyncKeyState('A') ){
-				printf("a키입력");
+			if (GetAsyncKeyState('A')) {
 			}
 		}
 
 		if (escPressed) {
-			printf("\n프로그램이 종료됩니다.\n");
+			cout << "\n프로그램이 종료됩니다.\n" << endl;
 			break;//1번째 while문 종료
 		}
 
-		printf("\n");
+		cout << "\n" << endl;
 	}
 
 	return 0;
